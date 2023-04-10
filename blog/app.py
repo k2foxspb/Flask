a83e45059cd3b5bbda70b4ba9b3f163bd4ab5ebd
k2foxspb <k2foxpsb@mail.ru>
2023-04-10 09:59:24 +0300

    logger, BadRequest

--- blog/app.py
+++ blog/app.py
@@ -55,3 +55,17 @@ def process_after_request(response):
         response.headers['process_time'] = time() - g.start_time
 
     return response
+
+
+@app.route("/power/")
+def power_value():
+    x = request.args.get("x") or ""
+    y = request.args.get("y") or ""
+    if not (x.isdigit() and y.isdigit()):
+        app.logger.info("invalid values for power: x=%r and y=%r", x, y)
+        raise BadRequest("please pass integers in `x` and `y` query params")
+    x = int(x)
+    y = int(y)
+    result = x ** y
+    app.logger.debug("%s ** %s = %s", x, y, result)
+    return str(result)
