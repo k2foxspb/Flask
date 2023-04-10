b81538f5e78600bbe4c18c119beb4836b570633a
k2foxspb <k2foxpsb@mail.ru>
2023-04-10 09:59:48 +0300

    ZeroDivisionError

--- blog/app.py
+++ blog/app.py
@@ -69,3 +69,15 @@ def power_value():
     result = x ** y
     app.logger.debug("%s ** %s = %s", x, y, result)
     return str(result)
+
+
+@app.route("/divide-by-zero/")
+def do_zero_division():
+    return 1 / 0
+
+
+@app.errorhandler(ZeroDivisionError)
+def handle_zero_division_error(error):
+    print(error)  # prints str version of error: 'division by zero'
+    app.logger.exception("Here's traceback for zero division error")
+    return "Never divide by zero!", 400
