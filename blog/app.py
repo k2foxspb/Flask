03111f20d97a693d5ed959be2e24a89ace863c27
k2foxspb <k2foxpsb@mail.ru>
2023-04-10 09:54:27 +0300

    query string

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from time import time
+
+from flask import Flask, request, g
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello():
+    return 'Hello, Web!'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/greet/<name>/')
+def index(name: str):
+    return f'Hello, {name}!'
