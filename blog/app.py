21186ce2e854fc31bdd68a7ce926ab5f05fbe54e
k2foxspb <k2foxpsb@mail.ru>
2023-04-10 09:57:52 +0300

    query string +

--- blog/app.py
+++ blog/app.py
@@ -14,3 +14,27 @@ def hello():
 @app.route('/greet/<name>/')
 def index(name: str):
     return f'Hello, {name}!'
+
+
+@app.route('/user/')
+def read_user():
+    name = request.args.get('name')
+    surname = request.args.get('surname')
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
+
+
+@app.route('/status/', methods=['GET', 'Post'])
+def costum_status_code():
+    if request.method == 'GET':
+        return '''\
+        To get response with custom status code
+send request using POST method
+and pass `code` in JSON body / FormData
+'''
+    print('raw bytes data:', request.data)
+
+    if request.form and 'code' in request.form:
+        return 'code from form', request.form['code']
+    if request.json and "code" in request.json:
+        return "code from json", request.json["code"]
+    return "", 204
