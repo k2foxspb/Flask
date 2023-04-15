cb8b666fea9b3ea54478bac8c7b187ca74807191
k2foxspb <k2foxpsb@mail.ru>
2023-04-15 11:38:59 +0300

    views, templates

--- blog/views/users.py
+++ blog/views/users.py
@@ -13,3 +13,13 @@ USERS = {
 @users_app.route('/', endpoint='list')
 def user_list():
     return render_template('users/list.html', users=USERS)
+
+
+@users_app.route("/<int:user_id>/", endpoint="details")
+def user_details(user_id: int):
+    try:
+        user_name = USERS[user_id]
+    except KeyError:
+        raise NotFound(f"User #{user_id} doesn't exist!")
+    return render_template('users/details.html', user_id=user_id,
+                           user_name=user_name)
